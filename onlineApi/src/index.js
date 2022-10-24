ad0f5f31a31ab4b5f298dac0d36ad9af9c43a9fd
unknown <samsadalam2131@gmail.com>
2022-10-25 00:01:30 +0530

    govt

--- onlineApi/src/index.js
+++ onlineApi/src/index.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-// const hbs = require("hbs")
-const port = 4000;
+const hbs = require("hbs")
+const port = 8000;
 
 
 // this line of code is called from the websit public folder=============
@@ -11,30 +11,30 @@ const port = 4000;
 const staticPath = path.join(__dirname, "../public");
 
 // this line of code is called from the websit public folder=============
-// const templatePath = path.join(__dirname, "../templates/views")
-// const partialsPath = path.join(__dirname, "../templates/partials")
+const templatePath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
 // to set the viwes ingine========================================================================
 app.set("view engine", "hbs");
-// app.set("views",templatePath);
-// hbs.registerPartials(partialsPath);
+app.set("views",templatePath);
+hbs.registerPartials(partialsPath);
 
 // this line of code is called from the websit public folder=============
-// app.use(express.static(staticPath));
+app.use(express.static(staticPath));
 // this line of code is called from the websit public folder=============
 
 
 // template engine route============
 app.get("/", (req, res) => {
-res.render('hello bro');
+res.render('index');
 });
 // for the about page=================
 app.get ("/MOCK TESTS", (req , res ) => {
     res.render("mock test");
     });
 // for the contact page=================
-    // app.get ("/contact", (req , res ) => {
-    //     res.render("contact");
-    //     });
+    app.get ("/contact", (req , res ) => {
+        res.render("contact");
+        });
 // for the services page=================
         // app.get ("/services", (req , res ) => {
         //     res.render("services");
